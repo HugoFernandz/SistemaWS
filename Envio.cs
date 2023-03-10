97358121a9b6251d7900d64505f1c08208a9933e
victor fernandes <victor.hugo@megalaudo.com.br>
2023-03-10 14:48:59 -0300

    Adicionar arquivos de projeto.

---
+++ Envio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaWS
+{
+    public class Envio
+    {
+        public string idLogin { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
---
+++ WebService/Consulta.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SistemaWS.WebService
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class ConsultaWs : System.Web.Services.WebService
+    {
+        #region Consulta 01
+        [WebMethod(Description = "Retorna STRING", MessageName = "Consulta01")]
+        public string Consulta_01(string strTexto)
+        {
+            return strTexto;
+        }
+        #endregion
+
+        #region Consulta 02
+        [WebMethod(Description = "Retorna STRING", MessageName = "Consulta02")]
+        public string Consulta_02(Envio envio)
+        {
+            if (envio.Nome == "Victor")
+            {
+                return "Sucesso";
+            }
+            else
+            {
+                return "Erro";
+            }
+        }
+        #endregion
+    }
+}
